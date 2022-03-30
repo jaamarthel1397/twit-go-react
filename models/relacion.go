aadf7b163c79f1fd73280cb5f828d88da98dcf15
Jorge <jaamarthel@gmail.com>
2022-03-30 14:38:12 -0600

    AltaRelacion

---
+++ models/relacion.go
@@ -0,0 +1,7 @@
+package models
+
+/* Relacion model para grabar la relacion de un usuario */
+type Relacion struct {
+	UsuarioID         string `bson:"usuarioid" json:"usuarioId"`
+	UsuarioRelacionID string `bson:"usuariorelacionid" json:"usuarioRelacionId"`
+}
